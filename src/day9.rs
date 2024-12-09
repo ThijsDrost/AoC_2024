56ae9478f0cd1e0be353e16934c75fffb94912de
drost <drosttj@gmail.com>
2024-12-09 23:07:37 +0100

    day 9 part 2

--- src/day9.rs
+++ src/day9.rs
@@ -47,5 +47,58 @@ pub fn day9(base_path: &str, real: bool) -> (usize, usize) {
         }
     }
 
-    (num, 0)
+    let mut holes: Vec<(u32, u32)> = Vec::new();
+    let mut items: Vec<(u32, u32)> = Vec::new();
+    let mut indexes = 0;
+
+    for (index, value) in char_vec.iter().enumerate() {
+        if index % 2 == 0 {
+            items.push((indexes, *value));
+            indexes += value;
+        }
+        else {
+            holes.push((indexes, *value));
+            indexes += value;
+        }
+    }
+
+    let mut total2 = 0;
+    let mut found = None;
+    for (item_index, item) in items.iter().enumerate().rev() {
+        found = None;
+        for (hole_index, hole) in holes.iter_mut().enumerate(){
+            if item.0 < hole.0 {
+                break;
+            }
+            if item.1 <= hole.1 {
+                for value in hole.0..(hole.0+item.1) {
+                    total2 += item_index*value as usize;
+                }
+                hole.0 += item.1;
+                hole.1 -= item.1;
+                found = Some((item.0, item.1, hole_index));
+                break;
+            }
+        }
+        if let Some((idx, length, hole_index)) = found {
+            for i in hole_index..holes.len() {
+                if idx < holes[i].0 {
+                    if (idx + length) == holes[i].0 {
+                        holes[i].0 = idx;
+                        holes[i].1 += length;
+                    }
+                    else {
+                        holes.insert(i, (idx, length));
+                    }
+                    break;
+                }
+            }
+        }
+        else {
+            for value in item.0..(item.0 + item.1) {
+                total2 += item_index * value as usize;
+            }
+        }
+    }
+    (num, total2)
 }
